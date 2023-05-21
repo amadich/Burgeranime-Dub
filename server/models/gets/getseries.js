8d3489f3a561a090c0b88314a50071023455f274
amadich <74735976+amadich@users.noreply.github.com>
2023-05-22 00:02:38 +0100

    Add files via upload

--- server/models/gets/getseries.js
+++ server/models/gets/getseries.js
@@ -1,11 +1,22 @@
 const animeModel = require("../CreateAnime");
 
 const getseries = (app) => {
-   app.get("/getseries" , async (req, res) => {
-         const { serieID }  = req.query;
-         console.log(serieID);
-         const anime = await animeModel.findOne({_id:serieID});
-         res.status(200).json({anime});
-   })
-}
+  app.get("/getseries", async (req, res) => {
+    try {
+      const { serieID } = req.query;
+      console.log(serieID);
+      const anime = await animeModel.findOne({ _id: serieID });
+
+      if (!anime) {
+        return res.status(404).json({ error: "Anime not found" });
+      }
+
+      res.status(200).json({ anime });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
+};
+
 module.exports = getseries;
