1cdd708f1affe2d2d54c5a495e5bd078eaad3998
Jeridi Mohamed <74735976+amadich@users.noreply.github.com>
2023-05-25 02:18:06 +0100

    Add files via upload

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,106 +1,99 @@
 import { Link } from 'react-router-dom';
-import LOGO from '../../public/burgeranime.png'
+import LOGO from '../../public/burgeranime.png';
 import { useState } from 'react';
 import { useCookies } from 'react-cookie';
 import Axios from 'axios';
 
 function Login() {
-   
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-   const [email,setEmail] = useState("");
-   const [pwd,setPwd] = useState("");
+  const [_, setCookie] = useCookies(['acc_tokens']);
 
-   const [_,setCookie] = useCookies(["acc_tokens"]);
-   
-   const handsubmit = (e) => {
-      e.preventDefault();
-      const serverURL = "http://localhost:3001";
-      Axios.post(`${serverURL}/signin`, {email,pwd})
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const serverURL = 'http://localhost:3001';
+    setError('');
+
+    Axios.post(`${serverURL}/signin`, { email, pwd: password })
       .then((res) => {
-         if (res.data.ok == 1) {
-            // Exclude "pwd" property from User object
-            const { pwd, ...userWithoutPwd } = res.data.User;
-            setCookie("acc_tokens",res.data.token);
-            window.localStorage.setItem("token",res.data.token);
-            /*
-            window.localStorage.setItem("User", JSON.stringify(userWithoutPwd));
-            
-            window.localStorage.setItem("name",res.data.User.name);
-            window.localStorage.setItem("email",res.data.User.email);
-            window.localStorage.setItem("userID",res.data.User._id);
-            */
-
-                  window.location.href = "/";
-                  console.log(res.data);
-
-         }
-
-         else if (res.data.ok == 4) {
-            console.log(res.data.message);
-         }
-
-         else {
-            console.log(res.data.message);
-         }
-
+        if (res.data.ok === 1) {
+          //const { pwd, ...userWithoutPwd } = res.data.user;
+          setCookie('acc_tokens', res.data.token);
+          window.localStorage.setItem('token', res.data.token);
+          window.location.href = '/';
+          console.log(res.data);
+        } else {
+          setError(res.data.message || 'An error occurred. Please try again later.');
+        }
       })
-   }
-   
+      .catch((error) => {
+        console.error('Error logging in:', error);
+        setError('An error occurred. Please try again later.');
+      });
+  };
 
-   return ( 
-      <>
-  
-               <div className="hero min-h-screen  mt-10 absolute">
-                  <div className="hero-content flex-col ">
-                     <div className="text-center lg:text-left ">
-                        <Link to="/" ><h1 className="text-5xl font-bold "><img src={LOGO} alt="Burger Anime" width={100} className='ml-auto' /></h1> </Link>
-                        
-                     </div>
-                           <div className="card-body">
-                           <form onSubmit={handsubmit}>
-                                   
-                                 <div className="form-control">
-                                       <label className="label">
-                                          <span className="">Email</span>
-                                       </label>
-                                       <input type="text" required placeholder="email" className="input input-bordered bg-[#000000ab]" onChange={(e) => {setEmail(e.target.value)}} />
-                                    </div>
-                                    <div className="form-control">
-                                       <label className="label">
-                                          <span className="">Password</span>
-                                       </label>
-                                       <input type="password" required placeholder="password" className="input input-bordered bg-[#000000ab]" onChange={(e) => {setPwd(e.target.value)}} />
-                                       <label className="label">
-                                          <a href="#" className=" link link-hover text-xs">Forgot password?</a>
-                                       </label>
-                                    </div>
-                                    <div className="form-control mt-6">
-                                       
-                                       <button className="btn text-black hover:text-white hover:bg-green-600 bg-yellow-500">Log IN</button>
-                                    </div>
-                              </form>
-                        
-                        </div>
-                        
-                        <div className='text-center p-10'>
-
-                           <p>
-                              No account? <span className='text-yellow-500'>
-                              <Link to="/register">Create One  </Link>
-                              </span>
-                              </p>
-
-                              </div>
-                     </div>
-                    
-                  </div>
-
-         
-               
-
-            
-      </>
-    );
+  return (
+    <>
+      <div className="hero min-h-screen mt-10 absolute">
+        <div className="hero-content flex-col">
+          <div className="text-center lg:text-left">
+            <Link to="/">
+              <h1 className="text-5xl font-bold">
+                <img src={LOGO} alt="Burger Anime" width={100} className="ml-auto" />
+              </h1>
+            </Link>
+          </div>
+          <div className="card-body">
+            <form onSubmit={handleSubmit}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="">Email</span>
+                </label>
+                <input
+                  type="email"
+                  required
+                  placeholder="email"
+                  className="input input-bordered bg-[#000000ab]"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="">Password</span>
+                </label>
+                <input
+                  type="password"
+                  required
+                  placeholder="password"
+                  className="input input-bordered bg-[#000000ab]"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <label className="label">
+                  <a href="#" className="link link-hover text-xs">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              {error && <div className="text-red-500 mt-4">{error}</div>}
+              <div className="form-control mt-6">
+                <button className="btn text-black hover:text-white hover:bg-green-600 bg-yellow-500">Log In</button>
+              </div>
+            </form>
+          </div>
+          <div className="text-center p-10">
+            <p>
+              No account?{' '}
+              <span className="text-yellow-500">
+                <Link to="/register">Create One</Link>
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,106 +1,111 @@
 import { Link } from 'react-router-dom';
-import Registeryuzu from '../../public/assets/register-yuzu.png'
-import LOGO from '../../public/burgeranime.png'
-import Axios from "axios";
+import Registeryuzu from '../../public/assets/register-yuzu.png';
+import LOGO from '../../public/burgeranime.png';
+import Axios from 'axios';
 import { useState } from 'react';
 import { useCookies } from 'react-cookie';
-function Rejester() {
-   const [name,setName] = useState("");
-   const [email,setEmail] = useState("");
-   const [pwd,setPwd] = useState("");
 
-   const [_,setCookie] = useCookies(["acc_tokens"]);
+function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [error, setError] = useState('');
 
-   const handsubmit = (e) => {
-      e.preventDefault();
-      const serverURL = "http://localhost:3001";
-      Axios.post(`${serverURL}/createuser`, {name,email,pwd})
+  const [_, setCookie] = useCookies(['acc_tokens']);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const serverURL = 'http://localhost:3001';
+
+    // Validate email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setError('Invalid email format');
+      return;
+    }
+
+    // Check for strange or invalid names
+    const nameRegex = /^[a-zA-Z\s]+$/;
+    if (!nameRegex.test(name)) {
+      setError('Invalid name');
+      return;
+    }
+
+    Axios.post(`${serverURL}/createuser`, { name, email, pwd })
       .then((res) => {
-         
-         if (res.data.ok != 0) {
-            setCookie("acc_tokens",res.data.token);
-            
-           /*
-            window.localStorage.setItem("User",JSON.stringify(res.data.User));
-
-            window.localStorage.setItem("name",res.data.User.name);
-            window.localStorage.setItem("email",res.data.User.email);
-            */
-            window.localStorage.setItem("token",res.data.token);
-            console.log(res.data);
-
-            window.location.href = "/";
-         }
-
-         else {
-            console.log(res.data.message);
-         }
-
+        if (res.data.ok !== 0) {
+          setCookie('acc_tokens', res.data.token);
+          window.localStorage.setItem('token', res.data.token);
+          console.log(res.data);
+          window.location.href = '/';
+        } else {
+          setError(res.data.message);
+        }
       })
-   }
+      .catch((error) => {
+        console.error('Error registering:', error);
+        setError('An error occurred. Please try again later.');
+      });
+  };
 
-   return ( 
-      <>
-  
-               <div className="hero min-h-screen  mt-16 absolute">
-                  <div className="hero-content flex-col lg:flex-row-reverse">
-                     <div className="text-center lg:text-left ">
-                        <Link to="/" ><h1 className="text-5xl font-bold "><img src={LOGO} alt="Burger Anime" width={100} className='ml-auto' /></h1> </Link>
-                        
-                     </div>
-                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-[#343136ab]">
-                        <img src={Registeryuzu} alt="" width={100} className='ml-[80%] mt-[-8%]' />
-                        <div className="card-body">
-                           <form onSubmit={handsubmit}>
-                                    <div className="form-control">
-                                       <label className="label">
-                                          <span className="label-text">Username <i className='text-xs text-yellow-500'> ( Your Public Name in Burger Anime )</i></span>
-                                       </label>
-                                       <input type="text" required placeholder="Name" className="input input-bordered bg-[#000000ab]" onChange={(e) => {setName(e.target.value)}} />
-                                    </div>
-                                 <div className="form-control">
-                                       <label className="label">
-                                          <span className="label-text">Email</span>
-                                       </label>
-                                       <input type="text" required placeholder="email" className="input input-bordered bg-[#000000ab]" onChange={(e) => {setEmail(e.target.value)}} />
-                                    </div>
-                                    <div className="form-control">
-                                       <label className="label">
-                                          <span className="label-text">Password</span>
-                                       </label>
-                                       <input type="password" required placeholder="password" className="input input-bordered bg-[#000000ab]" onChange={(e) => {setPwd(e.target.value)}} />
-                                       <label className="label">
-                                          <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                       </label>
-                                    </div>
-                                    <div className="form-control mt-6">
-                                       <p className='text-xs text-center p-5 text-slate-500'>
-                                       By creating an account you're agreeing to our Terms & Privacy Policy , and you confirm that you are at least 16 years of age.
-                                       </p>
-                                       <button className="btn text-black hover:text-white hover:bg-green-600 bg-yellow-500">Create Account</button>
-                                    </div>
-                              </form>
-                        
-                        </div>
-                        
-                        <div className='text-center p-10'>
-
-                           <p>
-                              Already have an account? <span className='text-yellow-500'>
-                              <Link to="/login">LOG IN</Link>
-                              </span>
-                              </p>
-
-                              </div>
-                     </div>
-                    
-                  </div>
-
-               
-
+  return (
+    <>
+      <div className="hero min-h-screen mt-16 absolute">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <Link to="/">
+              <h1 className="text-5xl font-bold">
+                <img src={LOGO} alt="Burger Anime" width={100} className="ml-auto" />
+              </h1>
+            </Link>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-[#343136ab]">
+            <img src={Registeryuzu} alt="" width={100} className="ml-[80%] mt-[-8%]" />
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Username <i className="text-xs text-yellow-500">( Your Public Name in Burger Anime )</i></span>
+                  </label>
+                  <input type="text" required placeholder="Name" className="input input-bordered bg-[#000000ab]" onChange={(e) => { setName(e.target.value); }} />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input type="email" required placeholder="email" className="input input-bordered bg-[#000000ab]" onChange={(e) => { setEmail(e.target.value); }} />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input type="password" required placeholder="password" className="input input-bordered bg-[#000000ab]" onChange={(e) => { setPwd(e.target.value); }} />
+                  <label className="label">
+                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                  </label>
+                </div>
+                {error && <div className="text-red-500 mt-4">{error}</div>}
+                <div className="form-control mt-6">
+                  <p className="text-xs text-center p-5 text-slate-500">
+                    By creating an account you're agreeing to our Terms & Privacy Policy, and you confirm that you are at least 16 years of age.
+                  </p>
+                  <button className="btn text-black hover:text-white hover:bg-green-600 bg-yellow-500">Create Account</button>
+                </div>
+              </form>
             </div>
-      </>
-    );
+            <div className="text-center p-10">
+              <p>
+                Already have an account?{' '}
+                <span className="text-yellow-500">
+                  <Link to="/login">LOG IN</Link>
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Rejester;
+export default Register;
