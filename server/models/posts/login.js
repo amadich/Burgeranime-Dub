ae07bc0e43ba35224a6e49ca57a7727326707d21
Jeridi Mohamed <74735976+amadich@users.noreply.github.com>
2023-05-25 02:18:30 +0100

    Add files via upload

--- server/models/posts/login.js
+++ server/models/posts/login.js
@@ -1,28 +1,34 @@
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const UserModel = require("../CreateUser");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const UserModel = require('../CreateUser');
 
 const Login = (app) => {
-   app.post('/signin' , async (req,res) => {
-      
-      try {
-         const {email,pwd} = req.body;
-         const User = await UserModel.findOne({email});
-         !User && res.json({message: "Not Found This Email!",ok:0});
-         const checkpwd = await bcrypt.compare(pwd,User.pwd);
-         !checkpwd && res.json({message: "Email Or Password Incorrect",ok:4});
-
-         token = jwt.sign({User,ok:1},"shhh");
-
-         const decoded = jwt.verify(token, "shhh");
-         // 'decoded' now contains the decoded payload of the JWT token
-         console.log(decoded);
-
-         res.json({token,User,ok:1});
-      } catch (error) {
-         console.log("Error in Signin Post",error);
+  app.post('/signin', async (req, res) => {
+    try {
+      const { email, pwd } = req.body;
+      const user = await UserModel.findOne({ email });
+      console.log(pwd);
+      if (!user) {
+        return res.json({ message: 'Email not found', ok: 0 });
       }
 
-   })
-}
-module.exports  = Login
+      const isPasswordValid = await bcrypt.compare(pwd, user.pwd);
+
+      if (!isPasswordValid) {
+        return res.json({ message: 'Incorrect email or password', ok: 0 });
+      }
+
+      const token = jwt.sign({ User : user, ok: 1 }, 'shhh');
+
+      const decoded = jwt.verify(token, 'shhh');
+      console.log(decoded);
+
+      res.json({ token, user, ok: 1 });
+    } catch (error) {
+      console.log('Error in Signin Post', error);
+      res.status(500).json({ message: 'An error occurred. Please try again later.', ok: 0 });
+    }
+  });
+};
+
+module.exports = Login;
