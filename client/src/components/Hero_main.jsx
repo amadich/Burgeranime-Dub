b5dfcbe239469b47cf45c9fe5338ae71198b0b2b
Jeridi Mohamed <74735976+amadich@users.noreply.github.com>
2023-05-25 01:15:29 +0100

    Add files via upload

--- client/src/components/Hero_main.jsx
+++ client/src/components/Hero_main.jsx
@@ -3,11 +3,20 @@ function Hero_main() {
    return ( 
       <>
             <nav className="relative pt-16">
-            <div className="hero h-96 bg-cover " style={{ backgroundImage: `url("../public/assets/heros/bg-orange.jpg")` }}>
+            <div className="hero h-96 bg-cover " style={{ backgroundImage: `url("../public/assets/heros/bgAnime.png")` }}>
                <div className="hero-overlay bg-opacity-60"></div>
                <div className="hero-content text-center text-neutral-content">
-                  <div className="">
-                     <img src="../public/assets/heros/1ab56dff-df62-4094-b5f2-d17613ff4a39.webp" alt="anime name" width={600} />
+                  <div className="hidden lg:block">
+                     <h1 className="text-8xl font-bold pr-16">
+                        <span className="text-red-500"> BURGER </span> 
+                        <span className="inline-block">
+                           <img src="../public/burgeranime.png" alt="LOGO" width={50} />
+                        </span>
+                        <span className="text-white-500"> ANIME </span>
+                        </h1>
+                  </div>
+                  <div className="lg:hidden">
+                     <img src="../public/burgeranime.png" alt="LOGO" width={50} />
                   </div>
                </div>
             </div>
